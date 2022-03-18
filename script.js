d00c9a0eef198e8509cd619bee630105fb5a68ab
dainis <dainis.santars@gmail.com>
2022-03-18 15:20:41 +0200

    json pievienots

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const API = "https://kursi-chats.dainisantars.repl.co"
 
 let zina = document.querySelector('.manaZina');
 let zinas = document.querySelector('.chataZinas');
+let vards = document.querySelector('.vards');
 
 function sutitZinu()
 {
     console.log('sutitZinu() Darbojas');
     zinas.innerHTML = zinas.innerHTML + '<br />' + zina.value;
+    fetch(API + '/sutit/' + vards.value + '/' + zina.value)
 }
 
 async function ieladetChataZinas()
